b5cd35d98bec9fdc19476e1e1673d39d5870c9f7
potatoeee <muhammadhanif6390@gmail.com>
2024-11-22 16:00:08 +0800

    lol

--- CEG Database Project ABLE.c
+++ CEG Database Project ABLE.c
@@ -175,7 +175,17 @@ int main() {
     printf("Justin\n");
     printf("Xuande\n");
 
-    
+    while (1) {
+        printf("Type OPEN to open the student database.");
+        scanf(userInputRaw, sizeof(userInputRaw), stdin);
+        if (userInputRaw == "OPEN") {
+            return openFile;
+        }
+        else if (userInputRaw == NULL) {
+            printf("Wrong input bro");
+            continue;
+        }
+    }
     
     
     return 0;
